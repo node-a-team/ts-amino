ede0581e9a71abe67c746c45af08f6c09e620d26
yunjh1994 <yunjh1994@gmail.com>
2019-07-30 20:01:33 +0900

    Add codec to index path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as Amino from "./amino";
+import { Codec } from "./codec";
 import * as Encoder from "./encoder";
 import * as Options from "./options";
 import { Symbols, Typ3, Type } from "./type";
 
-export { Amino, Encoder, Type, Typ3, Symbols, Options };
+export { Amino, Codec, Encoder, Type, Typ3, Symbols, Options };
